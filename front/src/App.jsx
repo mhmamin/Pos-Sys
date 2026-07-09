184b26fed825acc4befb8399f251a1733f246b8f
mhmamin <aminmohammedmasri@gmail.com>
2026-07-09 16:06:18 +0300

    delete

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./componants/Header";
 import { AuthProvider } from "./context/AuthContext";
 // import ProtecteRoute
-import ProtecteRoute from "./componants/ProtecteRoute";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
