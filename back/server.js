3c7ac6a5e7800d576a976aba5b0a124f19343f3a
mhmamin <aminmohammedmasri@gmail.com>
2026-07-09 13:18:28 +0300

    fix

--- back/server.js
+++ back/server.js
@@ -27,7 +27,10 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "pos-sys-git-main-amin-almasri.vercel.app",
+    ],
     credentials: true,
   }),
 );
