599c3b647c18b00080f3b7fc4d6b5ddfdafac308
mhmamin <aminmohammedmasri@gmail.com>
2026-07-12 02:18:46 +0300

    fix

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,10 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // إجبار الفيت على تخطي وتجاوز استدعاء axios الخارجي وعدم فحصه أثناء البناء
-      external: ["axios", "/vercel/path0/front/src/api/axios.js"],
-    },
-  },
 });
