5eb614db6bb4857d8aaffd13695e5da921236329
mhmamin <aminmohammedmasri@gmail.com>
2026-07-12 03:41:22 +0300

    .......

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    // إجبار الأداة على استخدام البناء القديم والمستقر
-    transformer: "postcss",
-  },
-  build: {
-    // استخدام esbuild لضغط الـ CSS بدلاً من lightningcss المزعج
-    cssMinify: "esbuild",
-    minify: "esbuild",
-  },
+  plugins: [react(), tailwindcss()],
 });
