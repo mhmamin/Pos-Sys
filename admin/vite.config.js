f4537fa39f8de0b12d6981c3a6ca6e15d0a47a2f
mhmamin <aminmohammedmasri@gmail.com>
2026-07-12 03:11:15 +0300

    commit44

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,11 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   css: {
-    lightningcss: {
-      ignoredRules: ["@theme", "@tailwind", "@layer"],
-    },
+    // إجبار الأداة على استخدام البناء القديم والمستقر
+    transformer: "postcss",
   },
   build: {
+    // استخدام esbuild لضغط الـ CSS بدلاً من lightningcss المزعج
     cssMinify: "esbuild",
+    minify: "esbuild",
   },
 });
