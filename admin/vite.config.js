affe01922ca57cfb3ec8c9507fd3866541ee3f48
mhmamin <aminmohammedmasri@gmail.com>
2026-07-12 03:02:46 +0300

    ..

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,5 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    transformer: "postcss",
+    lightningcss: false,
+  },
+  build: {
+    cssMinify: "esbuild",
+  },
 });
-// force rebuild 1
